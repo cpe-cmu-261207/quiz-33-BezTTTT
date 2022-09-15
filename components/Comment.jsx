082970b30b2d9bb911c357285c28d8da1c9bc105
BezTTTT <Best_1614@hotmail.com>
2022-09-15 15:26:52 +0700

    tummaipen

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,13 +1,41 @@
 import React from "react";
+import { comments } from "../libs/comments";
 import Reply from "./Reply";
 
 export default function Comment(props) {
   return (
     <div>
-      {/* Entire Comment div */}
-      <div></div>
-
-      {/*render Reply here... */}
+      <div className="d-flex gap-2 my-2">
+        <img
+          src={props.img}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#3A3B3C" }}
+        >
+          <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+            {props.user}
+          </span>
+          <br />
+          <span style={{ color: "#E4E6EB" }}>{props.com}</span>
+          <div className="d-flex align-items-center gap-1">
+            <img src="/like.svg" width={20}></img>
+            <span style={{ color: "#B0B3B8" }}>{props.like} </span>
+          </div>
+        </div>
+        {props.rep.map((x) => {
+          <Reply
+            user={x.username}
+            img={x.userImagePath}
+            com={x.replyText}
+            like={x.likeNum}
+          />;
+        })}
+      </div>
     </div>
   );
 }
--- components/Reply.jsx
+++ components/Reply.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function Reply(props) {
-  return <div></div>;
+  return (
+    <div className="d-flex gap-2 my-2 ps-5">
+      <img
+        src={x.img}
+        width="48"
+        height="48"
+        className="rounded-circle"
+        style={{ objectFit: "cover" }}
+      />
+      <div
+        className="rounded rounded-3 p-2"
+        style={{ backgroundColor: "#3A3B3C" }}
+      >
+        <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+          {x.user}
+        </span>
+        <br />
+        <span style={{ color: "#E4E6EB" }}>{x.com}</span>
+      </div>
+    </div>
+  );
 }
